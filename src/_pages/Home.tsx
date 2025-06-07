61b90562337b43a7251ff8545e003f4639b3bcab
Sushantmg <sushantmg2004@gmail.com>
2025-06-07 21:58:41 +0545

    home page

--- src/_pages/Home.tsx
+++ src/_pages/Home.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Hero from "@/_components/home/Hero";
+import Features from "@/_components/home/Features";
+import AD from "@/_components/home/AD";
 
-function Home() {
-  return (
-    <div>Home</div>
-  )
+export default function Home() {
+    return (
+        <main className="w-full relative overflow-hidden dark:bg-gray-900">
+            <Hero />
+
+            <div className="-mt-20 relative z-20 px-4 md:px-8 ">
+                <Features />
+            </div>
+
+            <div className="-mt-12 relative z-10">
+                <AD />
+            </div>
+        </main>
+    );
 }
-
-export default Home
