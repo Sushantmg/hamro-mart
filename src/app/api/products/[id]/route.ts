83b3050bdbfcedb7b3a31bdb97afa1b5cf405a9b
Sushantmg <sushantmg2004@gmail.com>
2025-06-22 06:56:28 +0545

    dynamic route product

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,13 +1,29 @@
-import { readFileSync } from "fs";
-import path from "path";
 import { NextResponse } from "next/server";
+import path from "path";
+import { promises as fs } from "fs";
 
-export async function GET(_: Request, { params }: { params: { id: string } }) {
+interface Product {
+  id: number;
+  name: string;
+  category: string;
+  image: string;
+  desc: string;
+  price: number;
+  discount: number;
+}
+
+// The GET function has a typed context parameter that includes params
+export async function GET(
+  request: Request,
+  context: { params: { id: string } }
+) {
+  const { id } = context.params;
+
   const filePath = path.join(process.cwd(), "data", "db.json");
-  const jsonData = readFileSync(filePath, "utf-8");
-  const data = JSON.parse(jsonData);
+  const jsonData = await fs.readFile(filePath, "utf-8");
+  const data: { products: Product[] } = JSON.parse(jsonData);
 
-  const product = data.products.find((p: any) => p.id === parseInt(params.id));
+  const product = data.products.find((p) => p.id === parseInt(id, 10));
 
   if (!product) {
     return NextResponse.json({ error: "Product not found" }, { status: 404 });
