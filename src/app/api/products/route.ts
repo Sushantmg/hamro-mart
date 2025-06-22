41c540ef906aee43a1f5d278fcc45dc94b4d391d
Sushantmg <sushantmg2004@gmail.com>
2025-06-22 06:54:24 +0545

    route product

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,9 +2,26 @@ import { NextResponse } from "next/server";
 import path from "path";
 import { promises as fs } from "fs";
 
-export async function GET() {
+interface Product {
+  id: number;
+  name: string;
+  category: string;
+  image: string;
+  desc: string;
+  price: number;
+  discount: number;
+}
+
+export async function GET(_: Request, { params }: { params: { id: string } }) {
   const filePath = path.join(process.cwd(), "data", "db.json");
   const jsonData = await fs.readFile(filePath, "utf-8");
   const data = JSON.parse(jsonData);
-  return NextResponse.json(data.products);
+
+  const product = data.products.find((p: Product) => p.id === parseInt(params.id));
+
+  if (!product) {
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+  }
+
+  return NextResponse.json(product);
 }
